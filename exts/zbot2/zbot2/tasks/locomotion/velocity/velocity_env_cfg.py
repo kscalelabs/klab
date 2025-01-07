6862814eb9d7979308fbbd706584190e29c8fc06
Ali K <ali@kscale.dev>
2025-01-07 22:48:45 +0000

    abalation 1, remove lin and ang vel, remove projected gravity, unify poses, remove height scanner

--- exts/zbot2/zbot2/tasks/locomotion/velocity/velocity_env_cfg.py
+++ exts/zbot2/zbot2/tasks/locomotion/velocity/velocity_env_cfg.py
@@ -118,34 +118,74 @@ class ObservationsCfg:
     class PolicyCfg(ObsGroup):
         """Observations for policy group."""
 
+        # Remove base_lin_vel and base_ang_vel
         # observation terms (order preserved)
-        base_lin_vel = ObsTerm(func=mdp.base_lin_vel, noise=Unoise(n_min=-0.1, n_max=0.1))
-        base_ang_vel = ObsTerm(func=mdp.base_ang_vel, noise=Unoise(n_min=-0.2, n_max=0.2))
-        projected_gravity = ObsTerm(
-            func=mdp.projected_gravity,
-            noise=Unoise(n_min=-0.05, n_max=0.05),
-        )
+        # base_lin_vel = ObsTerm(func=mdp.base_lin_vel, noise=Unoise(n_min=-0.1, n_max=0.1))
+        # base_ang_vel = ObsTerm(func=mdp.base_ang_vel, noise=Unoise(n_min=-0.2, n_max=0.2))
+
+        # Remove projected gravity
+        # projected_gravity = ObsTerm(
+        #     func=mdp.projected_gravity,
+        #     noise=Unoise(n_min=-0.05, n_max=0.05),
+        # )
+
         velocity_commands = ObsTerm(func=mdp.generated_commands, params={"command_name": "base_velocity"})
+
+        # # TODO: Find out where the IMU is and add to the code (raw accel)
+        # imu_ang_vel = ObsTerm(
+        #     func=mdp.imu_ang_vel,
+        #     noise=Unoise(n_min=-0.05, n_max=0.05),
+        # )
+        # imu_lin_acc = ObsTerm(
+        #     func=mdp.imu_lin_acc,
+        #     noise=Unoise(n_min=-0.05, n_max=0.05),
+        # )
+
+        # Remove "seperated" poses
         
         # Update hip position observations to use correct joint names
-        hip_yaw = ObsTerm(func=mdp.joint_pos_rel, noise=Unoise(n_min=-0.03, n_max=0.03),
-                          params={"asset_cfg": SceneEntityCfg("robot", joint_names=["left_hip_yaw", "right_hip_yaw"])})
-        hip_roll_pitch = ObsTerm(func=mdp.joint_pos_rel, noise=Unoise(n_min=-0.03, n_max=0.03),
-                          params={"asset_cfg": SceneEntityCfg("robot", joint_names=["left_hip_roll", "right_hip_roll", 
-                                                                                  "left_hip_pitch", "right_hip_pitch"])})
-        knee_pos = ObsTerm(func=mdp.joint_pos_rel, noise=Unoise(n_min=-0.05, n_max=0.05),
-                          params={"asset_cfg": SceneEntityCfg("robot", joint_names=["left_knee_pitch", "right_knee_pitch"])})
-        ankle_pos = ObsTerm(func=mdp.joint_pos_rel, noise=Unoise(n_min=-0.08, n_max=0.08),
-                          params={"asset_cfg": SceneEntityCfg("robot", joint_names=["left_ankle_pitch", "right_ankle_pitch"])})
+        # hip_yaw = ObsTerm(func=mdp.joint_pos_rel, noise=Unoise(n_min=-0.03, n_max=0.03),
+        #                   params={"asset_cfg": SceneEntityCfg("robot", joint_names=["left_hip_yaw", "right_hip_yaw"])})
+        # hip_roll_pitch = ObsTerm(func=mdp.joint_pos_rel, noise=Unoise(n_min=-0.03, n_max=0.03),
+        #                   params={"asset_cfg": SceneEntityCfg("robot", joint_names=["left_hip_roll", "right_hip_roll", 
+        #                                                                           "left_hip_pitch", "right_hip_pitch"])})
+        # knee_pos = ObsTerm(func=mdp.joint_pos_rel, noise=Unoise(n_min=-0.05, n_max=0.05),
+        #                   params={"asset_cfg": SceneEntityCfg("robot", joint_names=["left_knee_pitch", "right_knee_pitch"])})
+        # ankle_pos = ObsTerm(func=mdp.joint_pos_rel, noise=Unoise(n_min=-0.08, n_max=0.08),
+        #                   params={"asset_cfg": SceneEntityCfg("robot", joint_names=["left_ankle_pitch", "right_ankle_pitch"])})
+
+        # Unify joints and use positions
+        joint_angles = ObsTerm(
+            func=mdp.joint_pos_rel,
+            noise=Unoise(n_min=-0.05, n_max=0.05),
+            params={
+                "asset_cfg": SceneEntityCfg(
+                    "robot",
+                    joint_names=[
+                        "left_hip_yaw",
+                        "right_hip_yaw",
+                        "left_hip_roll",
+                        "right_hip_roll",
+                        "left_hip_pitch",
+                        "right_hip_pitch",
+                        "left_knee_pitch",
+                        "right_knee_pitch",
+                        "left_ankle_pitch",
+                        "right_ankle_pitch",
+                    ],
+                )
+            },
+        )
         joint_vel = ObsTerm(func=mdp.joint_vel_rel, noise=Unoise(n_min=-1.5, n_max=1.5))
         actions = ObsTerm(func=mdp.last_action)
 
-        height_scan = ObsTerm(
-            func=mdp.height_scan,
-            params={"sensor_cfg": SceneEntityCfg("height_scanner")},
-            noise=Unoise(n_min=-0.1, n_max=0.1),
-            clip=(-1.0, 1.0),
-        )
+        # Remove height scanner
+        # height_scan = ObsTerm(
+        #     func=mdp.height_scan,
+        #     params={"sensor_cfg": SceneEntityCfg("height_scanner")},
+        #     noise=Unoise(n_min=-0.1, n_max=0.1),
+        #     clip=(-1.0, 1.0),
+        # )
 
         def __post_init__(self):
             self.enable_corruption = True
