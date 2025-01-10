eece6afcd032737b64d14c80c5778fd92c56142f
budzianowski <budzianowski@gmail.com>
2025-01-09 20:44:10 -0800

    save terrain

--- scripts/sim_lab.py
+++ scripts/sim_lab.py
@@ -45,7 +45,14 @@ def get_gravity_orientation(quaternion):
 
 
 class Runner:
-    def __init__(self, embodiment: str, policy: ort.InferenceSession, config: Dict, render: bool = True):
+    def __init__(
+        self,
+        embodiment: str,
+        policy: ort.InferenceSession,
+        config: Dict,
+        render: bool = True,
+        terrain: bool = False,
+    ):
         """
         Initialize the MuJoCo runner.
 
@@ -54,6 +61,7 @@ class Runner:
             policy: The policy used for controlling the simulation
             config: The configuration object containing simulation settings
             render: Whether to render the simulation
+            terrain: Whether to render the terrain
         """
         self.policy = policy
         self.render = render
@@ -61,19 +69,23 @@ class Runner:
         self.framerate = 30
         
         # Initialize model
-        mujoco_model_path = f"resources/{embodiment}/robot_fixed.xml"
+        if terrain:
+            mujoco_model_path = f"resources/{embodiment}/robot_fixed_terrain.xml"
+        else:
+            mujoco_model_path = f"resources/{embodiment}/robot_fixed.xml"
         num_actions = len(config["observations"]["policy"]["joint_angles"]["params"]["asset_cfg"]["joint_names"])
-        
+
         self.model_info = {
             "sim_dt": config["sim"]["dt"],
             "sim_decimation": config["decimation"],
-            "tau_factor": [3] * num_actions,
+            "tau_factor": [1] * num_actions,
             "num_actions": num_actions,
             "robot_effort": [config["scene"]["robot"]["actuators"]["zbot2_actuators"]["effort_limit"]] * num_actions,
             "robot_stiffness": [config["scene"]["robot"]["actuators"]["zbot2_actuators"]["stiffness"][".*"]] * num_actions,
             "robot_damping": [config["scene"]["robot"]["actuators"]["zbot2_actuators"]["damping"][".*"]] * num_actions,
+            "action_scale": config["actions"]["joint_pos"]["scale"]
         }
-        
+
         self.model = mujoco.MjModel.from_xml_path(mujoco_model_path)
         self.model.opt.timestep = self.model_info["sim_dt"]
         self.data = mujoco.MjData(self.model)
@@ -92,6 +104,7 @@ class Runner:
             logger.warning("No default position found, using zero initialization")
             self.default = np.zeros(self.model_info["num_actions"])
         
+        # self.default += np.random.uniform(-0.03, 0.03, size=self.default.shape)
         # Set up joint mappings
         self._setup_joint_mappings(config)
         
@@ -114,11 +127,14 @@ class Runner:
         """Set up mappings between MuJoCo and Isaac joint names."""
         mujoco_joint_names = []
         mujoco.mj_step(self.model, self.data)
-        # for ii in range(1, len(self.data.ctrl) + 1):
-        for ii in range(0, len(self.data.ctrl)):
+
+        for ii in range(1, len(self.data.ctrl) + 1):
             mujoco_joint_names.append(self.data.joint(ii).name)
 
-        isaac_joint_names = config["observations"]["policy"]["joint_angles"]["params"]["asset_cfg"]["joint_names"]
+        isaac_joint_names = [
+            "L_Hip_Roll", "R_Hip_Roll", "L_Hip_Yaw", "R_Hip_Yaw", "L_Hip_Pitch", "R_Hip_Pitch",
+            "L_Knee_Pitch", "R_Knee_Pitch", "L_Ankle_Pitch", "R_Ankle_Pitch"
+        ]
 
         for ii in range(len(mujoco_joint_names)):
             logging.info(f"{mujoco_joint_names[ii]} -> {isaac_joint_names[ii]}")
@@ -132,7 +148,6 @@ class Runner:
             isaac_joint_names[i]: mujoco_joint_names[i]
             for i in range(len(isaac_joint_names))
         }
-
         self.test_mappings()
 
     def map_isaac_to_mujoco(self, isaac_position):
@@ -170,7 +185,6 @@ class Runner:
 
     def test_mappings(self):
         mujoco_position = np.array([-0.9, 0.2, -0.377, 0.796, 0.377, -0.9, 0.2, 0.377, -0.796, -0.377])
-
         isaac_position  = np.array([-0.9, -0.9, 0.2, 0.2, -0.377, 0.377, 0.796, -0.796, 0.377, -0.377])
 
         # Map positions
@@ -183,7 +197,7 @@ class Runner:
     def step(self, x_vel_cmd: float, y_vel_cmd: float, yaw_vel_cmd: float):
         """
         Execute one step of the simulation.
-        
+
         Args:
             x_vel_cmd: X velocity command
             y_vel_cmd: Y velocity command
@@ -208,9 +222,10 @@ class Runner:
 
             input_name = self.policy.get_inputs()[0].name
             curr_actions = self.policy.run(None, {input_name: obs.reshape(1, -1).astype(np.float32)})[0][0]
-            curr_actions = self.map_isaac_to_mujoco(curr_actions)
             self.last_action = curr_actions.copy()
 
+            curr_actions = curr_actions * self.model_info["action_scale"]
+            curr_actions = self.map_isaac_to_mujoco(curr_actions)
             self.target_q = curr_actions
 
             if self.render:
@@ -219,11 +234,10 @@ class Runner:
                 self.frames.append(self.viewer.read_pixels())
 
         # Generate PD control
-        # pfb30
-        tau = self.kps * (self.target_q  + self.default - q) - self.kds * dq
+        tau = self.kps * (self.default + self.target_q - q) - self.kds * dq
 
         # Clamp torques
-        tau = np.clip(tau, -self.tau_limit, self.tau_limit)
+        # tau = np.clip(tau, -self.tau_limit, self.tau_limit)
         
         self.data.ctrl = tau
         mujoco.mj_step(self.model, self.data)
@@ -244,26 +258,30 @@ class Runner:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Deployment script.")
     parser.add_argument("--embodiment", type=str, default="zbot2", help="Embodiment name.")
-    parser.add_argument("--sim_duration", type=float, default=10.0, help="Simulation duration in seconds.")
-    parser.set_defaults(render=True)
+    parser.add_argument("--sim_duration", type=float, default=1.5, help="Simulation duration in seconds.")
+    parser.add_argument("--model_path", type=str, default="init_model", help="Model path.")
+    parser.add_argument("--terrain", action="store_true", help="Render the terrain.")
+    parser.add_argument("--render", action="store_true", help="Render the terrain.")
     args = parser.parse_args()
 
     x_vel_cmd, y_vel_cmd, yaw_vel_cmd = 0.2, 0.0, 0.0
-    x_vel_cmd, yvel_cmd, yawvel_cmd = 0.3, 0.0, 0
-    policy = onnx.load("example_model/exported_new/policy.onnx")
-    policy = onnx.load("init_model/exported/policy.onnx")
+
+    args.model_path = "init_model"
+    policy = onnx.load(f"{args.model_path}/exported/policy.onnx")
+    policy = onnx.load(f"init_model/exported/policy.onnx")
 
     # In the run_mujoco function, replace the policy.run line with:
     session = ort.InferenceSession(policy.SerializeToString())
 
-    with open("example_model/params/env.yaml", "r") as f:
+    with open(f"{args.model_path}/params/env.yaml", "r") as f:
         config = yaml.load(f, Loader=yaml.Loader)
 
     runner = Runner(
         embodiment=args.embodiment,
         policy=session,
         config=config,
-        render=args.render
+        render=args.render,
+        terrain=args.terrain
     )
     
     for _ in tqdm(range(int(args.sim_duration / config["sim"]["dt"])), desc="Simulating..."):
