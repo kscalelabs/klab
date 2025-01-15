a482d0154777c097bb750669c8991e19ed167bfc
Ali K <ali@kscale.dev>
2025-01-15 00:55:53 +0000

    save update

--- exts/pendulum/pendulum/tasks/locomotion/velocity/velocity_env_cfg.py
+++ exts/pendulum/pendulum/tasks/locomotion/velocity/velocity_env_cfg.py
@@ -124,54 +124,6 @@ class ObservationsCfg:
     class PolicyCfg(ObsGroup):
         """Observations for policy group."""
 
-        # Remove base_lin_vel and base_ang_vel
-        # observation terms (order preserved)
-        # base_lin_vel = ObsTerm(func=mdp.base_lin_vel, noise=Unoise(n_min=-0.1, n_max=0.1))
-        # base_ang_vel = ObsTerm(func=mdp.base_ang_vel, noise=Unoise(n_min=-0.2, n_max=0.2))
-
-        # velocity_commands = ObsTerm(func=mdp.generated_commands, params={"command_name": "base_velocity"})
-
-        # TODO: Add angular velocity commands (low priority)
-
-        ######
-        # Add IMU values
-        ######
-
-        ## IMU euler angles
-        # kscale_imu_euler = ObsTerm(
-        #     func=mdp.kscale_imu_euler,
-        #     noise=Unoise(n_min=-0.02, n_max=0.02),  # optional noise
-        #     params={"sensor_cfg": SceneEntityCfg("kscale_imu_sensor")}
-        # )
-
-        # IMU quaternion
-        # kscale_imu_quat = ObsTerm(
-        #     func=mdp.kscale_imu_quat,
-        #     noise=Unoise(n_min=-0.02, n_max=0.02),  # optional noise
-        #     params={"sensor_cfg": SceneEntityCfg("kscale_imu_sensor")}
-        # )
-
-        # # Projected gravity
-        # projected_gravity = ObsTerm(
-        #     func=mdp.projected_gravity,
-        #     noise=Unoise(n_min=-0.05, n_max=0.05),
-        #     params={"asset_cfg": SceneEntityCfg("robot")}
-        # )
-
-        ## IMU linear acceleration
-        # kscale_imu_linear_acc = ObsTerm(
-        #     func=mdp.kscale_imu_lin_acc,
-        #     # noise=Unoise(n_min=-0.000001, n_max=0.000001),  # optional noise
-        #     params={"sensor_cfg": SceneEntityCfg("kscale_imu_sensor")}  
-        # )
-
-        ## IMU angular velocity
-        # kscale_imu_angular_vel = ObsTerm(
-        #     func=mdp.imu_ang_vel,
-        #     # noise=Unoise(n_min=-0.000001, n_max=0.000001),  # optional noise
-        #     params={"sensor_cfg": SceneEntityCfg("kscale_imu_sensor")}
-        # )
-
         # Unify joints and use positions
         joint_angles = ObsTerm(
             func=mdp.joint_pos_rel,
@@ -187,9 +139,6 @@ class ObservationsCfg:
                 )
             },
         )
-        # joint_vel = ObsTerm(func=mdp.joint_vel_rel, noise=Unoise(n_min=-1.5, n_max=1.5))
-        # actions = ObsTerm(func=mdp.last_action)
-
         def __post_init__(self):
             self.enable_corruption = True
             self.concatenate_terms = True
@@ -200,98 +149,6 @@ class ObservationsCfg:
 @configclass
 class RandomizationCfg:
     """Configuration for randomization."""
-
-#     # startup
-#     physics_material = EventTerm(
-#         func=mdp.randomize_rigid_body_material,
-#         mode="startup",
-#         params={
-#             "asset_cfg": SceneEntityCfg("robot", body_names=".*"),
-#             "static_friction_range": (0.2, 1.25),
-#             "dynamic_friction_range": (0.2, 1.25),
-#             "restitution_range": (0.0, 0.1),
-#             "num_buckets": 64,
-#         },
-#     )
-
-#     scale_all_link_masses = EventTerm(
-#         func=mdp.randomize_rigid_body_mass,
-#         mode="startup",
-#         params={"asset_cfg": SceneEntityCfg("robot", body_names=".*"), "mass_distribution_params": (0.9, 1.1),
-#                 "operation": "scale"},
-#     )
-
-#     add_base_mass = EventTerm(
-#         func=mdp.randomize_rigid_body_mass,
-#         mode="startup",
-#         params={"asset_cfg": SceneEntityCfg("robot", body_names="base"), "mass_distribution_params": (-1.0, 1.0),
-#                 "operation": "add"},
-#     )
-
-#     scale_all_joint_armature = EventTerm(
-#         func=mdp.randomize_joint_parameters,
-#         mode="startup",
-#         params={"asset_cfg": SceneEntityCfg("robot", joint_names=[".*"]), "armature_distribution_params": (1.0, 1.05),
-#                 "operation": "scale"},
-#     )
-
-#     add_all_joint_default_pos = EventTerm(
-#         func=mdp.randomize_joint_default_pos,
-#         mode="startup",
-#         params={"asset_cfg": SceneEntityCfg("robot", joint_names=[".*"]), "pos_distribution_params": (-0.05, 0.05),
-#                 "operation": "add"},
-#     )
-
-#     scale_all_joint_friction_model = EventTerm(
-#         func=mdp.randomize_joint_friction_model,
-#         mode="startup",
-#         params={"asset_cfg": SceneEntityCfg("robot", joint_names=[".*"]), "friction_distribution_params": (0.9, 1.1),
-#                 "operation": "scale"},
-#     )
-
-#     # reset
-#     base_external_force_torque = EventTerm(
-#         func=mdp.apply_external_force_torque,
-#         mode="reset",
-#         params={
-#             "asset_cfg": SceneEntityCfg("robot", body_names="base"),
-#             "force_range": (0.0, 0.0),
-#             "torque_range": (-0.0, 0.0),
-#         },
-#     )
-
-#     reset_base = EventTerm(
-#         func=mdp.reset_root_state_uniform,
-#         mode="reset",
-#         params={
-#             "pose_range": {"x": (-0.5, 0.5), "y": (-0.5, 0.5), "yaw": (-3.14, 3.14)},
-#             "velocity_range": {
-#                 "x": (-0.5, 0.5),
-#                 "y": (-0.5, 0.5),
-#                 "z": (-0.5, 0.5),
-#                 "roll": (-0.5, 0.5),
-#                 "pitch": (-0.5, 0.5),
-#                 "yaw": (-0.5, 0.5),
-#             },
-#         },
-#     )
-
-#     reset_robot_joints = EventTerm(
-#         func=mdp.reset_joints_by_scale,
-#         mode="reset",
-#         params={
-#             "position_range": (0.5, 1.5),
-#             "velocity_range": (0.0, 0.0),
-#         },
-#     )
-
-#     # interval
-#     push_robot = EventTerm(
-#         func=mdp.push_by_setting_velocity,
-#         mode="interval",
-#         interval_range_s=(10.0, 15.0),
-#         params={"velocity_range": {"x": (-0.5, 0.5), "y": (-0.5, 0.5)}},
-#     )
 
 
 @configclass
